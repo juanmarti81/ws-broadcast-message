58b8d5e2d48ee9e681dc93dd56373e0f1326ac07
Juan Marti <juanma.marti@gmail.com>
2023-01-02 16:20:18 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const WebSocket = require('ws');
+
+// Configuration: adapt to your environment
+const WEB_SOCKET_PORT = 8080;
+
+// Create & Start the WebSocket server
+const server = new WebSocket.Server({ port : WEB_SOCKET_PORT });
+
+// Register event for client connection
+server.on('connection', async function connection(ws) {
+  console.log("New client connected")
+  // broadcast on web socket when receving a Redis PUB/SUB Event
+  ws.on('message', (message) => {
+    server.broadcast(message);
+  })
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+
+});
+
+console.log("WebSocket server started at ws://locahost:"+ WEB_SOCKET_PORT);
